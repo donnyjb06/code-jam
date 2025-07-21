eb64b60dc430251896c5315713a2077a5f91b035
Donovan Jabbar <djabbar51@gmail.com>
2025-07-21 12:40:12 -0600

    feat: dynamically render tiles based on theme

--- front-end/src/components/Map/Map.jsx
+++ front-end/src/components/Map/Map.jsx
@@ -1,13 +1,15 @@
 import { useEffect, useRef, useState } from 'react';
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
+import useTheme from '../../hooks/useTheme';
 const apiKey = import.meta.env.VITE_MAP_TILER_API_KEY;
 
 const Map = () => {
   const mapContainer = useRef(null);
   const mapRef = useRef(null);
   const [locationReady, setLocationReady] = useState(false);
-  const [coords, setCoords] = useState([-84.388, 33.749]); // fallback: Atlanta
+  const [coords, setCoords] = useState([-84.388, 33.749]);
+  const { theme } = useTheme();
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
@@ -27,13 +29,33 @@ const Map = () => {
 
     mapRef.current = new maplibregl.Map({
       container: mapContainer.current,
-      style: `https://api.maptiler.com/maps/streets/style.json?key=${apiKey}`,
+      style: `https://api.maptiler.com/maps/${
+        theme === 'dark' ? 'streets-dark' : 'streets'
+      }/style.json?key=${apiKey}`,
       center: coords,
       zoom: 12,
     });
+    console.log(mapRef.style)
 
     new maplibregl.Marker().setLngLat(coords).addTo(mapRef.current);
   }, [locationReady, coords]);
+
+  useEffect(() => {
+    if (!mapRef.current) return;
+
+    const updateStyle = () => {
+      const newStyle = `https://api.maptiler.com/maps/${
+        theme === 'dark' ? 'streets-dark' : 'streets'
+      }/style.json?key=${apiKey}`;
+      mapRef.current.setStyle(newStyle);
+    };
+
+    if (mapRef.current.loaded()) {
+      updateStyle()
+    } else {
+      mapRef.current.once('load', updateStyle)
+    }
+  }, [theme]);
 
   return <div ref={mapContainer} style={{ width: '100vw', height: '100%' }} />;
 };
