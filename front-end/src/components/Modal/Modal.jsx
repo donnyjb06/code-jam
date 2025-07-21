48444f201d67e0106d3e1d3196708068eeeda9ac
Donovan Jabbar <djabbar51@gmail.com>
2025-07-21 14:50:54 -0600

    feat: center contents of modal

--- front-end/src/components/Modal/Modal.jsx
+++ front-end/src/components/Modal/Modal.jsx
@@ -1,32 +1,33 @@
-import React from "react";
-import "./Modal.css";
-import { FaChevronDown, FaChevronUp } from "react-icons/fa6";
-import Dropdown from "../Dropdown/index";
-import { AnimatePresence } from "motion/react";
-import { motion } from "motion/react";
+import React from 'react';
+import './Modal.css';
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa6';
+import Dropdown from '../Dropdown/index';
+import { AnimatePresence } from 'motion/react';
+import { motion } from 'motion/react';
 
 const Modal = ({ mode }) => {
   const [isOpen, setIsOpen] = React.useState(true);
   return (
     <>
-      <div className={`modal ${isOpen ? "modal_open" : ""}`}>
+      <div className={`modal ${isOpen ? 'modal_open' : ''}`}>
         <motion.button
           onClick={() => setIsOpen(false)}
-          className="modal__button modal__close-button"
+          className='modal__button modal__close-button'
           initial={{ x: 0, y: 0 }}
-          whileHover={{ x: 0, y: 5 }}
-        >
-          <FaChevronDown style={{ color: "var(--clr-foreground)" }} size={20} />
+          whileHover={{ x: 0, y: 5 }}>
+          <FaChevronDown style={{ color: 'var(--clr-foreground)' }} size={20} />
         </motion.button>
         <AnimatePresence>
           <motion.div
-            className="modal__form"
+            className='modal__form'
             initial={{ x: -300, opacity: 0 }}
             animate={{ x: 0, opacity: 1 }}
-            exit={{}}
-          >
-            {mode === "form" && (
+            exit={{}}>
+            {mode === 'form' && (
               <>
+                <h1 className='form__heading'>
+                  Select Your State Fuel Up & Find the Fun!
+                </h1>
                 <Dropdown />
               </>
             )}
@@ -36,12 +37,11 @@ const Modal = ({ mode }) => {
       <motion.button
         onClick={() => setIsOpen(true)}
         className={`modal__button modal__open-button ${
-          isOpen ? "modal__open-button_hidden" : ""
+          isOpen ? 'modal__open-button_hidden' : ''
         }`}
         initial={{ x: 0, y: 0 }}
-        whileHover={{ x: 0, y: -5 }}
-      >
-        <FaChevronUp style={{ color: "var(--clr-foreground)" }} size={20} />
+        whileHover={{ x: 0, y: -5 }}>
+        <FaChevronUp style={{ color: 'var(--clr-foreground)' }} size={20} />
       </motion.button>
     </>
   );
