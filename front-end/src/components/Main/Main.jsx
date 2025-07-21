347124e81edcf9a0dffdba2bcb3df921bfa25b1e
Donovan Jabbar <djabbar51@gmail.com>
2025-07-21 12:40:42 -0600

    WIP: commit changes for teammate to collaborate

--- front-end/src/components/Main/Main.jsx
+++ front-end/src/components/Main/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Main.css';
 import Map from '../Map/Map';
+import Modal from '../Modal/Modal';
 
 const Main = () => {
   return (
     <main className='main'>
+      <Modal mode="form"/>
       <Map />
     </main>
   );
